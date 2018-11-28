c23dcca0ce5c9d9584cf2ec97d7a91471fc44362
govind <govind.gupta@innoplexus.com>
2018-11-28 19:40:10 +0530

    Added mustache.js for template rendering.

--- public/js/index.js
+++ public/js/index.js
@@ -7,11 +7,14 @@ socket.on('disconnect', function(){
 });
 //listener
 socket.on('newMessage', function (message) {
-  console.log('newMessage ', message);
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
-  jQuery('#messages').append(li);
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    text: message.text,
+    createdAt: formattedTime
+  });
+  jQuery('#messages').append(html);
 });
 
 /*socket.emit('createMessage', {
@@ -24,12 +27,13 @@ socket.on('newMessage', function (message) {
 socket.on('newLocationMessage', function (message) {
   console.log(message, message);
   var formattedTime = moment(message.createAt).format('h:mm a');
-  let li = jQuery('<li></li>');
-  let a = jQuery('<a target="_blank"> My Current Location </a>');
-  li.text(`${message.from} ${formattedTime}`);
-  a.attr('href', message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    createdAt: formattedTime,
+    url: message.url
+  });
+  jQuery('#messages').append(html);
 });
 
 
