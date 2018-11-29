dd00acd62f5aa07b99c8e9066d221d60f247acdc
govind <govind.gupta@innoplexus.com>
2018-11-29 10:39:30 +0530

    Added login page.

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,7 +3,7 @@ let socket = io();
 function scrollToBottom () {
   // Selectors
   var messages = jQuery('#messages');
-  var newMessage = messages.children('li:last-child')
+  var newMessage = messages.children('li:last-child');
   // Heights
   var clientHeight = messages.prop('clientHeight');
   var scrollTop = messages.prop('scrollTop');
